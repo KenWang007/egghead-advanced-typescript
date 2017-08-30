f4699aa9c983318b608110d0708ca6cb2ce09298
Zac Jones <zacjones93@gmail.com>
2017-08-30 15:06:52 -0700

    updated for plunker

--- index.ts
+++ index.ts
@@ -1,6 +1,28 @@
-const obj: { [key: string]: string } = {};
-obj.hasOwnProperty("foo");
-obj.foo = "bar";
+enum ShirtSize {
+  XS,
+  S,
+  M,
+  L,
+  XL
+}
 
-console.log(obj.foo)
+function assertNever(value: never): never {
+  // throw Error(`Unexpected value '${value}'`)
+  // Adjusted for plunker output
+  console.log(Error(`Unexpected value '${value}'`));
+}
 
+function prettyPrint(size: ShirtSize) {
+  switch (size) {
+      case ShirtSize.S: console.log("small");
+      case ShirtSize.M: return "medium";
+      case ShirtSize.L: return "large";
+      case ShirtSize.XL: return "extra large";
+      // [ts] Argument of type 'ShirtSize.XS' is
+      // not assignable to parameter of type 'never'.
+      default: return assertNever(size);
+  }
+}
+
+prettyPrint(ShirtSize.S)
+prettyPrint(ShirtSize.XXL)
