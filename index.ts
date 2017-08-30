b423d628e71a84c02af7df8e9852540961f3ee28
Zac Jones <zacjones93@gmail.com>
2017-08-30 14:24:46 -0700

    updated for plunker

--- index.ts
+++ index.ts
@@ -1,38 +1,9 @@
-// flow analysis
-function trimAndLower(text: string | null | undefined) {
-  // (parameter) text: string | null | undefined
-  // text;
+const numbers = [0, 1, 2, [3, 4], 5, [6], [7], 8, [9]];
 
-  if (typeof text === "string") {
-      // (parameter) text: string
-      // text;
-
-      return text.trim().toLowerCase();
-  }
-  // (parameter) text: null | undefined
-  // text;
-
-  console.log(text);
+function isFlat<T>(array: (T | T[])[]): array is T[] {
+    console.log(!array.some(Array.isArray));
 }
 
-console.log(trimAndLower(" LearnTypeScript.io "));
-
-
-let foo: number;
-
-// [ts] Variable 'foo' is used before being assigned.
-console.log(foo)
-
-
-
-
-// definite assignment analysis
-let bar: number | undefined;
-let bar = 42;
-
-// let bar: number
-// this will allow autocompletion
-bar
-
-console.log(bar)
-
+if (isFlat(numbers)) {
+    numbers;
+}
