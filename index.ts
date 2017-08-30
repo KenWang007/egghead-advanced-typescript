eca1f77011ee190aa41deebae3249e5e3c0ef3c0
Zac Jones <zacjones93@gmail.com>
2017-08-30 11:46:43 -0700

    modified for plunker

--- index.ts
+++ index.ts
@@ -1,17 +1,38 @@
-// non-nullable types
-
+// flow analysis
 function trimAndLower(text: string | null | undefined) {
+  // (parameter) text: string | null | undefined
+  // text;
+
   if (typeof text === "string") {
+      // (parameter) text: string
+      // text;
+
       return text.trim().toLowerCase();
   }
-  return text;
+  // (parameter) text: null | undefined
+  // text;
+
+  console.log(text);
 }
 
 console.log(trimAndLower(" LearnTypeScript.io "));
-console.log(trimAndLower(null));
-console.log(trimAndLower(undefined));
 
-// ========================================
 
-const container = document.getElementById("app-container")!;
-container.remove();
+let foo: number;
+
+// [ts] Variable 'foo' is used before being assigned.
+console.log(foo)
+
+
+
+
+// definite assignment analysis
+let bar: number | undefined;
+let bar = 42;
+
+// let bar: number
+// this will allow autocompletion
+bar
+
+console.log(bar)
+
