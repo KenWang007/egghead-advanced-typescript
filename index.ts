dda7c4fc474b522d24339b26e6ae3eff30afca51
Zac Jones <zacjones93@gmail.com>
2017-08-31 10:17:04 -0700

    updated for plunker

--- index.ts
+++ index.ts
@@ -1,24 +1,13 @@
-/**
- * Reverses the given string.
- * @param string The string to reverse.
- */
-function reverse(string: string): string;
-
-/**
- * Reverses the given array.
- * @param array The array to reverse.
- */
-function reverse<T>(array: T[]): T[];
-
-function reverse(stringOrArray: string | any[]) {
-    return typeof stringOrArray === "string" 
-    ? [...stringOrArray].reverse().join("") 
-    : stringOrArray.slice().reverse();
+const enum MediaTypes {
+  JSON = "application/json"
 }
 
-// [ts] const reversedString: string 
-const reversedString = reverse("TypeScript");
-// [ts] const reversedArray: number[]
-const reversedArray = reverse([4, 8, 15, 16, 23, 42]);
-console.log(reversedString)
-console.log(reversedArray)
+fetch("https://swapi.co/api/people/1/", {
+  headers: {
+      Accept: MediaTypes.JSON
+  }
+})
+.then((res) => res.json())
+.then(response => {
+ console.log(response.name)
+});
